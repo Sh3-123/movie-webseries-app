9abcc90441593c0b050230164c42e2cc453733d6
“[shawn]” <[shawnserrao3@gmail.com]>
2026-02-19 18:17:57 +0530

    Configure for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -20,8 +20,10 @@ app.get('/', (req, res) => {
     res.send('Movie Web Series API is running');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
 
 module.exports = app;
